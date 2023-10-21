692f2c27f975579b5fe6d5e2a0ee6e0b38cd541b
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-20 20:09:58 -0400

    Add style

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,24 @@ const App = () => {
   const [people, setPeople] = useState(data);
 
   return (
-    <>
-      <h2>{people.length} Birthdays Today</h2>
-      <div>
-        {people.map((person) => {
-          return <li key={person.id}>{person.name}</li>;
-        })}
+    <main>
+      <div className="container">
+        <h3>{people.length} Birthdays Today</h3>
+        <section className="">
+          {people.map((person) => {
+            return (
+              <article className="person" key={person.id}>
+                <img className="img" src={person.image} alt={person.name} />
+                <div>
+                  <h4>{person.name}</h4>
+                  <p>{person.age} years</p>
+                </div>
+              </article>
+            );
+          })}
+        </section>
       </div>
-    </>
+    </main>
   );
 };
 export default App;
