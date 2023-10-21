48aefca107c02fe62c93009d7edc014aa8bba980
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-20 20:01:49 -0400

    render person

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import data from "./data";
+import { useState } from "react";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <>
+      <h2>{people.length} Birthdays Today</h2>
+      <div>
+        {people.map((person) => {
+          return <li key={person.id}>{person.name}</li>;
+        })}
+      </div>
+    </>
+  );
 };
 export default App;
