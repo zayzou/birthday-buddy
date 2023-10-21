7fc635ea5a8b4a198780d8f08b0aa462eaf4833f
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-20 20:52:39 -0400

    Implement clear all button

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { useState } from "react";
 
 const App = () => {
   const [people, setPeople] = useState(data);
+
   return (
     <main>
       <div className="container">
         <h3>{people.length} Birthdays Today</h3>
         <List people={people} />
+        <button type="button" className="btn btn-block" onClick={()=>setPeople([])}>Clear all</button>
       </div>
     </main>
   );
